265184c26b13b468bcb64fbc57fcecc7765e57c8
Makesh <makeshdev1995@gmail.com>
2024-12-14 19:12:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Righteous: ["Righteous", "sans-serif"],
+      Saira: ["Saira", "sans-serif"],
+      Rowdies: ["Rowdies", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
